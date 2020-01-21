e065f92a0dec34d57a975866916042a04faedd7d
Yukikatase <arasiusu@gmail.com>
2020-01-21 15:16:30 +0900

    コードを書く方も追加

---
+++ problem.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+import heapq
+import math
+import bisect
+from itertools import permutations
+from itertools import accumulate
+from fractions import gcd
+def input():
+    return sys.stdin.readline()[:-1]
+mod=10**9+7
