259441dbaa53a11ee751fc69d500802da6c91538
Yukikatase <arasiusu@gmail.com>
2020-01-23 17:35:50 +0900

    直積(重複順列を追加)

--- problem.py
+++ problem.py
@@ -3,7 +3,7 @@ from collections import *
 import heapq
 import math
 import bisect
-from itertools import permutations,accumulate,combinations_with_replacement
+from itertools import permutations,accumulate,combinations_with_replacement,product
 from fractions import gcd
 def input():
     return sys.stdin.readline()[:-1]
