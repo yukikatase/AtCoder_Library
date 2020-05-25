529918c4c4d7f1c55d74864c6d0c06beef434f9d
Yukikatase <arasiusu@gmail.com>
2020-05-25 19:09:12 +0900

    sys.setrecursionlimitを追加

--- problem.py
+++ problem.py
@@ -4,13 +4,14 @@ import heapq
 import math
 import sys
 from collections import *
+from functools import lru_cache
 from itertools import accumulate, combinations, permutations, product
 from math import gcd
 def input():
     return sys.stdin.readline()[:-1]
 def ruiseki(lst):
     return [0]+list(accumulate(lst))
+sys.setrecursionlimit(500000)
 mod=pow(10,9)+7
 al=[chr(ord('a') + i) for i in range(26)]
 direction=[[1,0],[0,1],[-1,0],[0,-1]]
-
