2ce4c76ecfef4a6eb8f4c3fffd1fcd9d0d8f1b99
Yukikatase <arasiusu@gmail.com>
2020-08-06 01:00:40 +0900

    ライブラリも更新

--- problem.py
+++ problem.py
@@ -1,7 +1,4 @@
-import bisect
-import copy
-import heapq
-import math
+import bisect, copy, heapq, math
 import sys
 from collections import *
 from functools import lru_cache
@@ -10,8 +7,9 @@ def input():
     return sys.stdin.readline()[:-1]
 def ruiseki(lst):
     return [0]+list(accumulate(lst))
-sys.setrecursionlimit(500000)
+sys.setrecursionlimit(5000000)
 mod=pow(10,9)+7
 al=[chr(ord('a') + i) for i in range(26)]
 direction=[[1,0],[0,1],[-1,0],[0,-1]]
+
 
