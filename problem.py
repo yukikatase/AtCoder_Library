be59c2fd5b3bc253cc4aea9847f25c2ece6df7d4
Yukikatase <arasiusu@gmail.com>
2020-08-08 00:40:23 +0900

    テンプレにceilを追加

--- problem.py
+++ problem.py
@@ -1,5 +1,4 @@
-import bisect, copy, heapq, math
-import sys
+import bisect, copy, heapq, math, sys
 from collections import *
 from functools import lru_cache
 from itertools import accumulate, combinations, permutations, product
@@ -7,6 +6,8 @@ def input():
     return sys.stdin.readline()[:-1]
 def ruiseki(lst):
     return [0]+list(accumulate(lst))
+def celi(a,b):
+    return -(-a//b)
 sys.setrecursionlimit(5000000)
 mod=pow(10,9)+7
 al=[chr(ord('a') + i) for i in range(26)]
